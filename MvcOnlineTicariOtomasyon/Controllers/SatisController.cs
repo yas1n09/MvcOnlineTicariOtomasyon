e1f3ec0af0c619eb3b45bb328131afc498b4301b
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-07-22 17:32:09 +0300

    -

--- MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -20,15 +20,68 @@ namespace MvcOnlineTicariOtomasyon.Controllers
         [HttpGet]
         public ActionResult YeniSatis()
         {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.UrunID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            List<SelectListItem> deger2 = (from x in c.Caris.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " +x.CariSoyad,
+                                               Value = x.CariID.ToString()
+                                           }).ToList();
+            ViewBag.dgr2 = deger2;
+
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd +" "+ x.PersonelSoyad,
+                                               Value = x.PersonelID.ToString()
+                                           }).ToList();
+            ViewBag.dgr3 = deger3;
             return View();
         }
 
         [HttpPost]
         public ActionResult YeniSatis(SatisHareket s)
         {
+            s.SatisTarih =DateTime.Parse( DateTime.Now.ToShortDateString());
             c.SatisHarekets.Add(s);
             c.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult SatisGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.UrunID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            List<SelectListItem> deger2 = (from x in c.Caris.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd + " " + x.CariSoyad,
+                                               Value = x.CariID.ToString()
+                                           }).ToList();
+            ViewBag.dgr2 = deger2;
+
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.PersonelID.ToString()
+                                           }).ToList();
+            ViewBag.dgr3 = deger3;
+            var deger = c.SatisHarekets.Find(id);
+            return View("SatisGetir", deger);
         }
     }
 }
