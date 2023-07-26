4b061b5977f6dbadf15ee920a52e4ac4d9ed3c58
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-07-26 14:22:44 +0300

    Added new theme

---
+++ MvcOnlineTicariOtomasyon/Controllers/IstatistikController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        Context c = new Context();
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            var deger1 = c.Caris.Count().ToString();
+            ViewBag.d1 = deger1;
+            return View();
+        }
+    }
+}
