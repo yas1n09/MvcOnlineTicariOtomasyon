1deac1dba94b29874261d91af23c8cafacbd5fb7
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-08-09 19:38:04 +0300

    added cargo page

--- MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
@@ -17,5 +17,31 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var kargolar = c.KargoDetays.ToList();
             return View(kargolar);
         }
+        [HttpGet]
+        public ActionResult YeniKargo()
+        {
+            Random rnd = new Random();
+            string[] karakterler = { "A", "B", "C", "D", "E", "G", "H", "K"};
+            int k1, k2, k3;
+            k1 = rnd.Next(0, karakterler.Length);
+            k2 = rnd.Next(0, karakterler.Length);
+            k3 = rnd.Next(0, karakterler.Length);
+            int s1, s2, s3;
+            s1 = rnd.Next(100, 1000);
+            s2 = rnd.Next(10, 99);
+            s3 = rnd.Next(10, 99);
+            string kod = s1.ToString() + karakterler[k1] + s2 + karakterler[k2] + s3 + karakterler[k3];
+            ViewBag.takipkod = kod;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKargo(KargoDetay d)
+        {
+            c.KargoDetays.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
