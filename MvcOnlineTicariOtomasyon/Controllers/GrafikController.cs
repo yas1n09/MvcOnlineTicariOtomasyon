133a45dbf859162fdba1ded317659a4d466d538d
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-08-10 18:11:32 +0300

    Added Visual Charts-graphics (pie, Column ect.)

---
+++ MvcOnlineTicariOtomasyon/Controllers/GrafikController.cs
@@ -0,0 +1,114 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GrafikController : Controller
+    {
+        // GET: Grafik
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            var grafikciz = new Chart(600, 600);
+            grafikciz.AddTitle("Kategori - Ürün Stok Sayısı").AddLegend("Stok").AddSeries("Değerler", xValue: new[] { "Beyaz Esya", "Küçük Ev Aleti", "Mobilya " }, yValues: new[] { 85, 66, 98 }).Write();
+            return File(grafikciz.ToWebImage().GetBytes(), "image/jpeg");
+        }
+        Context c = new Context();
+        public ActionResult Index3()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+
+            var sonuclar = c.Uruns.ToList();
+            sonuclar.ToList().ForEach(x => xvalue.Add(x.UrunAd));
+            sonuclar.ToList().ForEach(y => yvalue.Add(y.Stok));
+
+            var grafik = new Chart(width: 1000, height: 500)
+                .AddTitle("Stoklar")
+                .AddSeries(chartType: "Pie", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(grafik.ToWebImage().GetBytes(), "image/jpeg");
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+        public ActionResult VisualizeUrunResult()
+        {
+            return Json(UrunListesi(), JsonRequestBehavior.AllowGet);
+        }
+        public List<Sinif1> UrunListesi()
+        {
+            List<Sinif1> snf = new List<Sinif1>();
+            snf.Add(new Sinif1()
+            {
+                urunad = "Bilgisayar",
+                stok = 120
+            });
+            snf.Add(new Sinif1()
+            {
+                urunad = "Beyyaz Eşya",
+                stok = 150
+            });
+            snf.Add(new Sinif1()
+            {
+                urunad = "Mobilya",
+                stok = 70
+            });
+            snf.Add(new Sinif1()
+            {
+                urunad = "Küçük Ev Aletleri",
+                stok = 180
+            });
+            snf.Add(new Sinif1()
+            {
+                urunad = "Mobil Cihazlar",
+                stok = 90
+            });
+            return snf;
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizeUrunResult2()
+        {
+            return Json(UrunListesi2(), JsonRequestBehavior.AllowGet);
+        }
+        public List<sinif2>UrunListesi2()
+        {
+            List<sinif2> snf = new List<sinif2>();
+            using (var context=new Context())
+            {
+                snf = c.Uruns.Select(x => new sinif2
+                {
+                    urn = x.UrunAd,
+                    stk = x.Stok
+                }).ToList();
+            }
+            return snf;
+        }
+        public ActionResult Index6()
+        {
+            return View();
+        }
+
+        public ActionResult Index7()
+        {
+            return View();
+        }
+
+    }
+    
+}
