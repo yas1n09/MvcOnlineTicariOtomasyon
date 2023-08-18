84c46a25799f8ecc1a3944d8f1ed4124d434bb8a
yasinyagci0990@gmail.com <yasinyagci0990@gmail.com>
2023-08-18 18:05:22 +0300

    Added Logout , messaging and  package tracking

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
@@ -20,7 +21,7 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.m = Mail;
             return View(degerler);
         }
-
+        [Authorize]
         public ActionResult Siparislerim()
         {
             var mail = (string)Session["CariMail"];
@@ -28,11 +29,12 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             var degerler = c.SatisHarekets.Where(x => x.CariID == id).ToList();
             return View(degerler);
         }
+        [Authorize]
         public ActionResult GelenMesajlar()
         {
             var mail = (string)Session["CariMail"];
 
-            var mesajlar = c.Mesajlars.Where(x=>x.Alici==mail).ToList();
+            var mesajlar = c.Mesajlars.Where(x => x.Alici == mail).OrderByDescending(x => x.MesajID).ToList();
             var gelensayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
             var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
 
@@ -40,17 +42,19 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             ViewBag.d1 = gelensayisi;
             return View(mesajlar);
         }
+        [Authorize]
         public ActionResult GidenMesajlar()
         {
             var mail = (string)Session["CariMail"];
             var gelensayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
             ViewBag.d1 = gelensayisi;
-            var mesajlar = c.Mesajlars.Where(x => x.Gonderici == mail).ToList();
+            var mesajlar = c.Mesajlars.Where(x => x.Gonderici == mail).OrderByDescending(z => z.MesajID).ToList();
             var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
 
             ViewBag.d2 = gidensayisi;
             return View(mesajlar);
         }
+        [Authorize]
         public ActionResult MesajDetay(int id)
         {
             var degerler = c.Mesajlars.Where(x => x.MesajID == id).ToList();
@@ -68,18 +72,57 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 
 
 
-
+        [Authorize]
         [HttpGet]
         public ActionResult YeniMesaj()
         {
+
+            var mail = (string)Session["CariMail"];
+            var gelensayisi = c.Mesajlars.Count(x => x.Alici == mail).ToString();
+            ViewBag.d1 = gelensayisi;
+            var gidensayisi = c.Mesajlars.Count(x => x.Gonderici == mail).ToString();
+            ViewBag.d2 = gelensayisi;
+
             return View();
         }
-
+        [Authorize]
         [HttpPost]
         public ActionResult YeniMesaj(Mesajlar m)
         {
+            var mail = (string)Session["CariMail"];
+            m.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            m.Gonderici = mail;
+
+            c.Mesajlars.Add(m);
+            c.SaveChanges();
+
             return View();
         }
+        [Authorize]
+        public ActionResult KargoTakip(string p)
+        {
 
+            var k = from x in c.KargoDetays select x;
+
+                k = k.Where(y => y.TakipKodu == p);
+
+            return View(k.ToList());
+
+            
+        }
+        [Authorize]
+        public ActionResult CariKargoTakip(string id)
+        {
+            var degerler = c.KargoTakips.Where(x => x.TakipKodu == id).ToList();
+
+            return View(degerler);
+        }
+        [Authorize]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
     }
 }
